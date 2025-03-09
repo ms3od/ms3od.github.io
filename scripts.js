33e060b3adf0d0ba895a11c642f64a039b4c6369
Younes Aboudrar <149315659+ms3od@users.noreply.github.com>
2025-03-09 16:22:47 +0000

    New update of my website

--- scripts.js
+++ scripts.js
@@ -1,29 +1,9 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
       });
     });
-  });
-  
-  // Scroll animations
-  const sections = document.querySelectorAll('section');
-  
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-        }
-      });
-    },
-    {
-      threshold: 0.1,
-    }
-  );
-  
-  sections.forEach((section) => {
-    observer.observe(section);
   });
